425e370a5cc0f32e33ee006fc0f708e192289e30
jedomor <jedomor111@g.educaand.es>
2023-02-24 13:17:29 +0100

    commit 24/02/2023

---
+++ UD5_07/src/main/java/com/mycompany/ud5_07/EscrituraFichero.java
@@ -0,0 +1,72 @@
+package com.mycompany.ud5_07;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+import java.util.*;
+
+public class EscrituraFichero {
+
+    private String nombreFichero = "";
+    // La clase File representa un fichero y su ruta completa en el sistema
+    // de ficheros del sistema operativo.
+    private File fichero;
+    // La clase FileWriter permite crear objetos que saben escribir caracteres
+    // a un fichero.
+    private FileWriter fw;
+    // La clase PrintWriter permite escribir líneas completas a un fichero de
+    // forma cómoda para el programador. Ejemplo: println()
+    private PrintWriter pw;
+    private boolean abierto = false;
+
+    public EscrituraFichero(String nombre) {
+        if (nombre != null) {
+            nombreFichero = nombre;
+        }
+    }
+
+    public void abrir() throws IOException {
+        fichero = new File(nombreFichero);
+        fw = new FileWriter(fichero);
+        pw = new PrintWriter(fw);
+        abierto = true;
+    }
+
+    public void cerrar() throws IOException {
+        if (abierto) {
+            pw.close();
+            fw.close();
+            abierto = false;
+        } else {
+            throw new IllegalStateException("El fichero no está abierto");
+        }
+    }
+
+    public void escribeTexto(String linea) {
+        if (abierto) {
+            if (linea != null) {
+                pw.println(linea);
+            }
+        } else {
+            throw new IllegalStateException("El fichero no está abierto");
+        }
+    }
+
+    public void escribeTexto(List<String>parrafo) throws IOException {
+        
+        if (abierto) {
+            Iterator<String>iter=parrafo.iterator();
+            while (iter.hasNext()) {
+                String linea = iter.next();
+                pw.println();
+                
+            }
+        } else {
+            throw new IllegalStateException("El fichero no está abierto");
+        }
+    }
+
+}
---
+++ UD5_07/src/main/java/com/mycompany/ud5_07/PruebaEscrituraFichero.java
@@ -0,0 +1,29 @@
+package com.mycompany.ud5_07;
+
+import java.io.IOException;
+import java.util.*;
+
+public class PruebaEscrituraFichero {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombreFich;
+        String frase;
+        int i=1;
+        List <String> lista = new ArrayList<>(Arrays.asList("arriba", "abajo","izquierda", "derecha"));
+                
+        // Recopilar información
+        System.out.println("Dime una frase y te la escribo a un fichero: ");
+        
+        do{
+            System.out.println("Dime la frase "+i +":");
+            frase = sc.nextLine();
+            if(!frase.equalsIgnoreCase("fin")){
+                lista.add(frase);
+            }
+            i++;
+        }while (!frase.equalsIgnoreCase("fin"));
+        
+
+    }
+}
